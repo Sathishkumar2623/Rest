f39084191862499de33d0618dedac0a01d1b59a6
SathishKumar2623 <sathishsurya2623@gmail.com>
2022-02-15 18:53:14 +0530

    created

---
+++ src/test/java/com/cts/City_classified_and_search/CityClassifiedAndSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.City_classified_and_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityClassifiedAndSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
